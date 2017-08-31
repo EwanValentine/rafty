4c5396e521767b930abc40124d07b7f1f3530970
Ewan Valentine <ewan.valentine89@gmail.com>
2017-08-31 23:27:51 +0100

    UPDATED to separate grpc code from raft code

--- main.go
+++ main.go
@@ -1,17 +1,10 @@
 package main
 
 import (
-	"errors"
 	"log"
-	"net"
-	"sync"
-	"time"
 
-	"golang.org/x/net/context"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/reflection"
+	"github.com/ewanvalentine/rafty/raft"
 
-	pb "github.com/ewanvalentine/rafty/proto"
 	"gopkg.in/alecthomas/kingpin.v2"
 )
 
@@ -26,152 +19,6 @@ var (
 	joinHostFlag   = join.Flag("host", "Host address").String()
 	joinLeaderFlag = join.Flag("leader", "Leader address").String()
 )
-
-const (
-	Candidate = "Candidate"
-	Follower  = "Follower"
-	Leader    = "Leader"
-)
-
-// Node
-type Node struct {
-	ID         int
-	Host       string
-	Attributes map[string]string
-	Status     string
-	Timeout    time.Time
-	Votes      int
-	client     pb.RaftyClient
-}
-
-type Rafty struct {
-	mutex sync.Mutex
-	Node
-
-	// List of all other Nodes in the network.
-	Nodes []Node
-}
-
-type RaftServer interface {
-	Start(host string)
-	AddNode(node Node) (Node, error)
-	Vote() error
-	Heartbeat() error
-}
-
-// Start -
-func (rafty *Rafty) Start(host string) {
-	lis, err := net.Listen("tcp", host)
-	if err != nil {
-		log.Fatalf("Could not start master node: %v", err)
-	}
-
-	s := grpc.NewServer()
-	pb.RegisterRaftyServer(s, &server{rafty})
-	reflection.Register(s)
-
-	// Only a leader should perform a heartbeat
-	if rafty.Status == Leader {
-		log.Println("Node is leader, performing heartbeat duties")
-		go func() {
-			heartbeat := time.Tick(1 * time.Second)
-			for {
-				select {
-				case <-heartbeat:
-					err := rafty.Heartbeat()
-					if err != nil {
-						log.Printf("Heartbeat error: %v", err)
-					}
-				}
-			}
-		}()
-	}
-
-	if err := s.Serve(lis); err != nil {
-		log.Fatalf("Failed to start master node server: %v", err)
-	}
-
-	log.Println("Started master node")
-}
-
-// Join -
-func (rafty *Rafty) Join(host, leader string) error {
-	_, err := rafty.AddNode(Node{Host: leader})
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Connecting to leader: %s", leader)
-
-	resp, err := rafty.Nodes[0].client.Join(context.Background(), &pb.JoinRequest{Host: host})
-	if err != nil {
-		return err
-	}
-
-	log.Printf("Joined leader on: %s - %d", host, resp.Id)
-	return nil
-}
-
-// AddNode -
-func (rafty *Rafty) AddNode(node Node) (Node, error) {
-
-	// This isn't sufficient, we need to update all other nodes as well
-	rafty.mutex.Lock()
-
-	// Form a connection to the new node
-	conn, err := grpc.Dial(node.Host, grpc.WithInsecure())
-	if err != nil {
-		return node, err
-	}
-
-	node.ID = len(rafty.Nodes)
-
-	// We need to run conn.Close() when that node dies
-	// or is removed somehow
-	// defer conn.Close()
-
-	node.client = pb.NewRaftyClient(conn)
-	rafty.Nodes = append(rafty.Nodes, node)
-	rafty.mutex.Unlock()
-
-	return node, nil
-}
-
-// Vote -
-func (rafty *Rafty) Vote() error {
-	if rafty.Status != Candidate {
-		return errors.New("you cannot vote for a node which is a non-candidate")
-	}
-	rafty.mutex.Lock()
-	rafty.Votes = rafty.Votes + 1
-
-	// This is potentially troubling, if one node dies
-	// and an election is performed, this will suffice
-	// as we take votes from all nodes, minus the one that died.
-	// However, if several nodes die in quick succession, this
-	// number may not add up. We need a better way to figure out
-	// the majority.
-	if rafty.Votes >= len(rafty.Nodes)-1 {
-		rafty.Status = Leader
-	}
-
-	rafty.mutex.Unlock()
-
-	return nil
-}
-
-// Heartbeat - poll all connected nodes with data
-func (rafty *Rafty) Heartbeat() error {
-	log.Println("Connected nodes: ", len(rafty.Nodes))
-	for _, node := range rafty.Nodes {
-		log.Printf("Data: %d", node.ID)
-		_, err := node.client.Heartbeat(context.Background(), &pb.HeartbeatRequest{Data: "test"})
-		if err != nil {
-			log.Printf("Dead node: %d - %v", node.ID, err)
-		}
-	}
-	return nil
-}
 
 // rafty create -host 127.0.0.1:8989
 // rafty join -host 127.0.0.1:8989
@@ -188,7 +35,7 @@ func main() {
 	case "join":
 		log.Printf("Adding node on address: %s", *joinHostFlag)
 
-		rafty := Rafty{}
+		rafty := raft.Rafty{}
 		rafty.Status = Follower
 		rafty.Votes = 0
 		rafty.Join(*joinHostFlag, *joinLeaderFlag)
@@ -197,7 +44,7 @@ func main() {
 	case "start":
 
 		// Start server
-		rafty := Rafty{}
+		rafty := raft.Rafty{}
 		rafty.Status = Leader
 		rafty.Votes = 0
 		rafty.Nodes = make([]Node, 0)
@@ -205,30 +52,4 @@ func main() {
 		log.Printf("Starting a node on address: %s", *startHostFlag)
 		rafty.Start(*startHostFlag)
 	}
-}
-
-type server struct {
-	rafty *Rafty
-}
-
-func (s *server) Join(ctx context.Context, req *pb.JoinRequest) (*pb.JoinResponse, error) {
-	node, err := s.rafty.AddNode(Node{Host: req.Host})
-	return &pb.JoinResponse{Id: int32(node.ID)}, err
-}
-
-func (s *server) List(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
-	return &pb.ListResponse{}, nil
-}
-
-func (s *server) Heartbeat(ctx context.Context, req *pb.HeartbeatRequest) (*pb.HeartbeatResponse, error) {
-	s.rafty.mutex.Lock()
-	s.rafty.Timeout = time.Now()
-	// Sync data/logs here
-	s.rafty.mutex.Unlock()
-
-	return &pb.HeartbeatResponse{Success: true}, nil
-}
-
-func (s *server) RequestVote(ctx context.Context, req *pb.RequestVoteRequest) (*pb.RequestVoteResponse, error) {
-	return &pb.RequestVoteResponse{Vote: true}, nil
 }
---
+++ raft/client.go
@@ -0,0 +1,163 @@
+package raft
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net"
+	"sync"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	pb "github.com/ewanvalentine/rafty/proto"
+	"github.com/ewanvalentine/rafty/transport"
+)
+
+const (
+	Candidate = "Candidate"
+	Follower  = "Follower"
+	Leader    = "Leader"
+)
+
+// Node
+type Node struct {
+	ID         int
+	Host       string
+	Attributes map[string]string
+	Status     string
+	Timeout    time.Time
+	Votes      int
+	client     pb.RaftyClient
+}
+
+type Rafty struct {
+	mutex sync.Mutex
+	Node
+
+	// List of all other Nodes in the network.
+	Nodes  []Node
+	server pb.RaftyServer
+}
+
+type RaftServer interface {
+	Start(host string)
+	AddNode(node Node) (Node, error)
+	Vote() error
+	Heartbeat() error
+}
+
+// Start -
+func (rafty *Rafty) Start(host string) {
+	lis, err := net.Listen("tcp", host)
+	if err != nil {
+		log.Fatalf("Could not start master node: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterRaftyServer(s, &transport.server{rafty})
+	reflection.Register(s)
+
+	// Only a leader should perform a heartbeat
+	if rafty.Status == Leader {
+		log.Println("Node is leader, performing heartbeat duties")
+		go func() {
+			heartbeat := time.Tick(1 * time.Second)
+			for {
+				select {
+				case <-heartbeat:
+					err := rafty.Heartbeat()
+					if err != nil {
+						log.Printf("Heartbeat error: %v", err)
+					}
+				}
+			}
+		}()
+	}
+
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("Failed to start master node server: %v", err)
+	}
+
+	log.Println("Started master node")
+}
+
+// Join -
+func (rafty *Rafty) Join(host, leader string) error {
+	_, err := rafty.AddNode(Node{Host: leader})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Connecting to leader: %s", leader)
+
+	resp, err := rafty.Nodes[0].client.Join(context.Background(), &pb.JoinRequest{Host: host})
+	if err != nil {
+		return err
+	}
+
+	log.Printf("Joined leader on: %s - %d", host, resp.Id)
+	return nil
+}
+
+// AddNode -
+func (rafty *Rafty) AddNode(node Node) (Node, error) {
+
+	// This isn't sufficient, we need to update all other nodes as well
+	rafty.mutex.Lock()
+
+	// Form a connection to the new node
+	conn, err := grpc.Dial(node.Host, grpc.WithInsecure())
+	if err != nil {
+		return node, err
+	}
+
+	node.ID = len(rafty.Nodes)
+
+	// We need to run conn.Close() when that node dies
+	// or is removed somehow
+	// defer conn.Close()
+
+	node.client = pb.NewRaftyClient(conn)
+	rafty.Nodes = append(rafty.Nodes, node)
+	rafty.mutex.Unlock()
+
+	return node, nil
+}
+
+// Vote -
+func (rafty *Rafty) Vote() error {
+	if rafty.Status != Candidate {
+		return errors.New("you cannot vote for a node which is a non-candidate")
+	}
+	rafty.mutex.Lock()
+	rafty.Votes = rafty.Votes + 1
+
+	// This is potentially troubling, if one node dies
+	// and an election is performed, this will suffice
+	// as we take votes from all nodes, minus the one that died.
+	// However, if several nodes die in quick succession, this
+	// number may not add up. We need a better way to figure out
+	// the majority.
+	if rafty.Votes >= len(rafty.Nodes)-1 {
+		rafty.Status = Leader
+	}
+
+	rafty.mutex.Unlock()
+
+	return nil
+}
+
+// Heartbeat - poll all connected nodes with data
+func (rafty *Rafty) Heartbeat() error {
+	log.Println("Connected nodes: ", len(rafty.Nodes))
+	for _, node := range rafty.Nodes {
+		log.Printf("Data: %d", node.ID)
+		_, err := node.client.Heartbeat(context.Background(), &pb.HeartbeatRequest{Data: "test"})
+		if err != nil {
+			log.Printf("Dead node: %d - %v", node.ID, err)
+		}
+	}
+	return nil
+}
